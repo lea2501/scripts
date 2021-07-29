4b64f176ba911e5f40d7edece57dd471175c5fec
Lea Chescotta <ochescotta@teco.com.ar>
2021-07-29 09:47:03 -0300

    Updated aur_update.sh and added initial devuan post installation scripts

--- aur_update.sh
+++ aur_update.sh
@@ -7,20 +7,19 @@
 
 #for dir in ~/aur/*; do (cd "$dir" && pwd && git pull); done
 
-for dir in ~/aur/*
-do
-    cd "$dir" || exit
-    #pwd
-    CURRENT_DIR=$(basename "$PWD")
-    git fetch
-    LOCAL=$(git rev-parse HEAD)
-    REMOTE=$(git rev-parse @{u})
-    if [ ! $LOCAL = $REMOTE ]; then
-        pwd
-	echo "Need to pull"
-        git pull
-        makepkg -sic --noconfirm
-    fi
-    rm -rf ./pkg/ ./src/ ./*.tar.* ./*.zip* ./*.tgz* ./*.bz* ./"${CURRENT_DIR}"
+for dir in ~/aur/*; do
+  cd "$dir" || exit
+  #pwd
+  CURRENT_DIR=$(basename "$PWD")
+  git fetch
+  LOCAL=$(git rev-parse HEAD)
+  REMOTE=$(git rev-parse @{u})
+  if [ ! $LOCAL = $REMOTE ]; then
+    pwd
+    echo "Need to pull"
+    git pull
+    makepkg -sic --noconfirm
+  fi
+  rm -rf ./pkg/ ./src/ ./*.tar.* ./*.zip* ./*.tgz* ./*.bz* ./"${CURRENT_DIR}"
 done
 exit 0
