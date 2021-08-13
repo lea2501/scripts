dfe869fed71120b6ec018bb0b1a5702761ddc2f0
Lea Chescotta <ochescotta@teco.com.ar>
2021-08-13 14:44:00 -0300

    Updated devuan post install scripts

--- devuan/post_install/devuan-post-install-script-01-set_basic_config-01-remove_sudo_password.sh
+++ devuan/post_install/devuan-post-install-script-01-set_basic_config-01-remove_sudo_password.sh
@@ -7,8 +7,9 @@ set -e
 
 echo "Removing asking for sudo password for user..."
 username=$(echo "$USER")
-echo "Using 'su -' command to gain root privileges..."
-su -
-echo "Using 'su -' command to gain root privileges... DONE"
-echo "${username} ALL=(ALL) NOPASSWD: ALL" | EDITOR='tee -a' visudo
+echo "To get sudo working execute following commands:"
+echo "$ su -"
+echo "  (Enter root password)"
+echo "# echo \"${username} ALL=(ALL) NOPASSWD: ALL\" | EDITOR='tee -a' visudo"
+echo "# exit"
 echo "Removing asking for sudo password for user... DONE"
