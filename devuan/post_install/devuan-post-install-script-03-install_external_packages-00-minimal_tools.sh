3e64916feadcc3252f1202a2ec6eebeb396d7f2d
Lea Chescotta <ochescotta@teco.com.ar>
2021-08-13 15:03:17 -0300

    Updated devuan post install scripts

--- devuan/post_install/devuan-post-install-script-03-install_external_packages-00-minimal_tools.sh
+++ devuan/post_install/devuan-post-install-script-03-install_external_packages-00-minimal_tools.sh
@@ -20,7 +20,7 @@ cloneRepo() {
 cloneRepo st https://git.suckless.org/st
 cp config.def.h config.def.h.bak
 curl -OL "https://raw.githubusercontent.com/lea2501/dotfiles/main/aur/st/config.def.h"
-mv config.h config.def.h
+sudo apt install -y libxft-dev
 sudo make clean install
 
 #cloneRepo dwm https://git.suckless.org/dwm
@@ -35,8 +35,8 @@ sudo make clean install
 #mv config.h config.def.h
 #sudo make clean install
 
-cloneRepo cwm https://github.com/zenlinux/cwm.git
-make
-sudo make PREFIX=/usr install
+#cloneRepo cwm https://github.com/zenlinux/cwm.git
+#make
+#sudo make PREFIX=/usr install
 cd || return
 curl -OL "https://raw.githubusercontent.com/lea2501/dotfiles/main/.cwmrc"
