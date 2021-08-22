3230bb57cfe8bbae8b4b0024f295f07b200c2a78
Lea Chescotta <ochescotta@teco.com.ar>
2021-08-22 09:49:57 -0300

    Improved post install scripts

---
+++ macos/post_install/macos-install-script-00-remove_sudo_password.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# fail if any commands fails
+set -e
+# debug log
+#set -x
+
+echo "Removing asking for sudo password for user..."
+echo "Open another terminal emulator and run command:"
+echo "  $ sudo visudo"
+echo "And change line:"
+echo "  %admin ALL=(ALL) ALL"
+echo "To"
+echo "  %admin ALL=(ALL) NOPASSWD: ALL"
+read -p "Press enter to continue"
+echo "Removing asking for sudo password for user... DONE"
