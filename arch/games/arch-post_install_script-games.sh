fda64f6d4ef2b69abcab9d62803911193a995ddb
Lea Chescotta <ochescotta@teco.com.ar>
2021-08-22 21:13:20 -0300

    Removed and updated scripts

--- arch/games/arch-post_install_script-games.sh
+++ arch/games/arch-post_install_script-games.sh
@@ -50,151 +50,64 @@ if [[ "$option" == "y" || "$option" == "Y" ]]; then
   cd || return
 
   # roguelikes
-  echo \
-    'nethack
-rogue
-stone-soup
-cataclysm-dda
-bsd-games
-angband
-infra-arcana
-umoria' >packages_games.txt
-
-  # roguelikes
-  echo \
-    'nethack
-rogue
-stone-soup
-cataclysm-dda
-bsd-games
-angband
-infra-arcana
-umoria' >packages_games.txt
+  paru -S nethack rogue stone-soup cataclysm-dda bsd-games angband infra-arcana umoria
 
   # doom
-  echo \
-    'chocolate-doom
-crispy-doom
-prboom-plus
-gzdoom' >packages_games.txt
+  paru -S chocolate-doom crispy-doom prboom-plus gzdoom
 
   # doom3
-  echo \
-    'dhewm3' >packages_games.txt
+  paru -S dhewm3
 
   # quake
-  echo \
-    'quakespasm
-qpakman' >packages_games.txt
+  paru -S quakespasm qpakman
 
   # quake2
-  echo \
-    'yamagi-quake2
-yamagi-quake2-rogue
-yamagi-quake2-xatrix' >packages_games.txt
+  paru -S yamagi-quake2 yamagi-quake2-rogue yamagi-quake2-xatrix
 
   # hexen2
-  echo \
-    'hexen2' >packages_games.txt
+  paru -S hexen2
 
   # half life
-  echo \
-    'xash3d-fwgs-git' >packages_games.txt
+  paru -S xash3d-fwgs-git
 
   # build games
-  echo \
-    'eduke32
-nblood
-raze' >packages_games.txt
+  paru -S eduke32 nblood raze
 
   # rtcw
-  echo \
-    'iortcw-git
-iortcw-data' >packages_games.txt
+  #paru -S iortcw-git iortcw-data
 
   # marathon
-  echo \
-    'alephone
-alephone-marathon
-alephone-marathon2
-alephone-rubiconx
-alephone-infinity' >packages_games.txt
+  paru -S alephone alephone-marathon alephone-marathon2 alephone-rubiconx alephone-infinity
 
   # descent
-  echo \
-    'd1x-rebirth
-  d2x-rebirth' >packages_games.txt
+  paru -S d1x-rebirth d2x-rebirth
 
   # diablo
-  echo \
-    'devilutionx' >packages_games.txt
+  paru -S devilutionx
 
   # uqm
-  echo \
-    'uqm
-uqm-sound
-uqm-remix' >packages_games.txt
+  paru -S uqm uqm-sound uqm-remix
 
   # minetest
-  echo \
-    'minetest
-minetest-server' >packages_games.txt
+  paru -S minetest minetest-server
 
   # applications
-  echo \
-    'flacon
-tsmuxer-git
-jdownloader2
-mangohud' >packages_games.txt
+  paru -S flacon tsmuxer-git jdownloader2 mangohud
 
   # torcs
-  echo \
-    'torcs
-torcs-data
-speed-dreams-svn' >packages_games.txt
+  paru -S torcs torcs-data speed-dreams-svn
 
   # emulators
-  echo \
-    'dosbox
-mgba
-snes9x
-mupen64plus
-dolphin-emu
-higan
-mednafen
-hatari
-ppsspp
-scummvm scummvm-tools
-fs-uae fs-uae-launcher
-sameboy
-nestopia
-pcsx2-git
-yuzu-git' >packages_games.txt
+  paru -S dosbox mgba snes9x mupen64plus dolphin-emu higan mednafen hatari ppsspp scummvm scummvm-tools fs-uae fs-uae-launcher sameboy nestopia pcsx2-git yuzu-git
 
   # wine
-  echo \
-    'wine wine-gecko wine-mono winetricks zenity vkd3d lutris' >packages_games.txt
+  paru -S wine wine-gecko wine-mono winetricks zenity vkd3d lutris
 
   # retroarch
-  echo \
-    'retroarch libretro
-libretro-dosbox-pure-git
-libretro-beetle-saturn-git
-libretro-opera-git' >packages_games.txt
+  paru -S retroarch libretro libretro-dosbox-pure-git libretro-beetle-saturn-git libretro-opera-git
 
   # games
-  echo \
-    'lbreakout2' >packages_games.txt
-
-  echo "Cleaning temporary data... "
-  for dir in ~/aur/*; do
-    cd "$dir" || exit
-    CURRENT_DIR=$(basename "$PWD")
-    rm -rf ./pkg/ ./src/ ./*.tar.* ./*.zip* ./*.tgz* ./*.bz* ./"${CURRENT_DIR}"
-  done
-  echo "Cleaning temporary data... DONE"
-
-  paru -S $(cat packages_games.txt)
+  paru -S lbreakout2
 
   echo "Installing games... DONE"
 fi
