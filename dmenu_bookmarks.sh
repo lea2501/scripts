189942d9c42f787f24f901de6ed850d724528dca
Lea Chescotta <ochescotta@teco.com.ar>
2021-09-13 10:00:54 -0300

    Improved bookmarks and terminfo_short scripts

--- dmenu_bookmarks.sh
+++ dmenu_bookmarks.sh
@@ -5,10 +5,11 @@ col_black="#000000"
 col_pink="#ff00f0"
 col_violet="#8f00ff"
 col_gray4="#eeeeee"
-#in=$(cat $HOME/Documentos/data/bookmarks | cut -d '-' -f 2 | dmenu -m 0 -fn ${dmenu_font} -nb ${col_black} -nf ${col_pink} -sb ${col_violet} -sf ${col_gray4} -p bookmarks:)     # dmenu input
-#match=$(cat $HOME/Documentos/data/bookmarks | grep $in | cut -d '-' -f 2)      # check match with ~/.bookmarks file
+#in=$(cat $HOME/Documentos/data/bookmarks | cut -d ',' -f 2 | dmenu -m 0 -fn ${dmenu_font} -nb ${col_black} -nf ${col_pink} -sb ${col_violet} -sf ${col_gray4} -l 5 -p bookmarks:)     # dmenu input
 in=$(cat $HOME/Documentos/data/bookmarks | dmenu -m 0 -fn ${dmenu_font} -nb ${col_black} -nf ${col_pink} -sb ${col_violet} -sf ${col_gray4} -l 5 -p bookmarks:)     # dmenu input
-match=$(cat $HOME/Documentos/data/bookmarks | grep $in)     # check match with ~/.bookmarks file
+match=$(cat $HOME/Documentos/data/bookmarks | grep $in | cut -d ',' -f 2)      # check match with ~/.bookmarks file
+#in=$(cat $HOME/Documentos/data/bookmarks | dmenu -m 0 -fn ${dmenu_font} -nb ${col_black} -nf ${col_pink} -sb ${col_violet} -sf ${col_gray4} -l 5 -p bookmarks:)     # dmenu input
+#match=$(cat $HOME/Documentos/data/bookmarks | grep $in)     # check match with ~/.bookmarks file
 
 if [ "$match" != "" ]; then
   surf -bdfgIS -a @ -z 1 $match        # exec bookmark url
