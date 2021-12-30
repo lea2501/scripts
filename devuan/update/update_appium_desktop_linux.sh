b51d18271efba48c941575e5e286c43340c4e4c5
Lea Chescotta <ochescotta@teco.com.ar>
2021-12-30 16:15:50 -0300

    Updated devuan update applications scripts

--- devuan/update/update_appium_desktop_linux.sh
+++ devuan/update/update_appium_desktop_linux.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# fail if any commands fails
+set -e
+# debug log
+#set -x
+
+# Set superuser privileges command if not set
+if [[ -z $su ]]; then
+  export su="sudo"
+fi
+
+$su apt-get update -qq
+$su apt-get install -qq -y curl
+
 cd || return
 mkdir -p ~/bin
 cd ~/bin || return
