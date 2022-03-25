ee185f07f33c3cb28809c1b572ad3aa5d3698d83
Lea Chescotta <ochescotta@teco.com.ar>
2022-03-25 08:13:55 -0300

    Added updateFirefoxUserJs.sh script

---
+++ updateFirefoxUserJs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# fail if any commands fails
+set -e
+# debug log
+#set -x
+
+repo="user.js"
+
+mkdir -p ~/src
+cd ~/src || return
+if [ ! -d "$repo" ]; then
+  git clone https://github.com/arkenfox/$repo.git
+  cd "$repo" || return
+else
+  cd "$repo" || return
+  git pull
+fi
+
+cp -v user.js ~/.mozilla/firefox/*default*/
