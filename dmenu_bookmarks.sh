ad836e4df49b1ea16a9fa332d2d9cbb5dc0fffe0
Lea Chescotta <ochescotta@teco.com.ar>
2022-04-03 20:02:13 -0300

    Updated dmenu_bookmarks.sh script

--- dmenu_bookmarks.sh
+++ dmenu_bookmarks.sh
@@ -6,13 +6,15 @@ col_pink="#ff00f0"
 col_violet="#8f00ff"
 col_gray4="#eeeeee"
 #in=$(cat $HOME/Documentos/data/bookmarks | cut -d ',' -f 2 | dmenu -m 0 -fn ${dmenu_font} -nb ${col_black} -nf ${col_pink} -sb ${col_violet} -sf ${col_gray4} -l 5 -p bookmarks:)     # dmenu input
-in=$(cat $HOME/Documentos/data/bookmarks | dmenu -m 0 -fn ${dmenu_font} -nb ${col_black} -nf ${col_pink} -sb ${col_violet} -sf ${col_gray4} -l 5 -p bookmarks:)     # dmenu input
+in=$(cat $HOME/Documentos/data/bookmarks | dmenu -m 0 -fn ${dmenu_font} -nb ${col_black} -nf ${col_pink} -sb ${col_violet} -sf ${col_gray4} -l 10 -p bookmarks:)     # dmenu input
 match=$(cat $HOME/Documentos/data/bookmarks | grep $in | cut -d ',' -f 2)      # check match with ~/.bookmarks file
 #in=$(cat $HOME/Documentos/data/bookmarks | dmenu -m 0 -fn ${dmenu_font} -nb ${col_black} -nf ${col_pink} -sb ${col_violet} -sf ${col_gray4} -l 5 -p bookmarks:)     # dmenu input
 #match=$(cat $HOME/Documentos/data/bookmarks | grep $in)     # check match with ~/.bookmarks file
 
 if [ "$match" != "" ]; then
-  surf -bdfgIS -a @ -z 1 $match        # exec bookmark url
+  #surf -bdfgIS -a @ -z 1 $match        # exec bookmark url
+  badwolf $match >/dev/null 2>&1    # exec bookmark url
 elif [ -n "$in" ]; then
-  surf -bdfgIS -a @ -z 1 "https://search.disroot.org/search?q=$in"     # search in searx
+  #surf -bdfgIS -a @ -z 1 "https://search.disroot.org/search?q=$in    # search in searx
+  badwolf "https://search.disroot.org/search?q=$in" >/dev/null 2>&1    # search in searx
 fi
