66a99d1956e8c22fc61f940be75c2a6c8f8fb407
Lea Chescotta <ochescotta@teco.com.ar>
2022-04-07 09:24:35 -0300

    Updated scripts

--- arch/post_install/install_other_browsers.sh
+++ arch/post_install/install_other_browsers.sh
@@ -7,5 +7,5 @@ set -e
 
 echo "Installing packages 'other browsers'..."
 #paru -S chromium firefox-esr-bin surf amfora bombadillo lagrange
-paru -S chromium firefox-esr-bin surf lagrange dooble-bin badwolf
+paru -S chromium firefox-esr-bin surf lagrange dooble-bin badwolf qutebrowser
 echo "Installing packages 'other browsers'... DONE"
--- openbsd/post_install/install_other_browsers.ksh
+++ openbsd/post_install/install_other_browsers.ksh
@@ -5,5 +5,6 @@ doas pkg_add chromium
 doas pkg_add surf
 doas pkg_add dooble
 doas pkg_add badwolf
+doas pkg_add qutebrowser
 #doas pkg_add amfora lagrange
 doas pkg_add tor tor-browser
