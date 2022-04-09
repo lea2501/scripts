e0a0f70b664e3735e9653f72be9bdec631af6585
Lea Chescotta <ochescotta@teco.com.ar>
2022-04-09 15:02:41 -0300

    Added updateBadwolfContentFilter.sh script to update badwolf adblocking json file

---
+++ updateBadwolfContentFilter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# fail if any commands fails
+set -e
+# debug log
+#set -x
+
+dir=$HOME/.config/badwolf
+file=content-filters.json
+url=https://easylist-downloads.adblockplus.org/easylist_min_content_blocker.json
+cd "$dir"
+curl -o "$file" -L $url
+ls -lahF "$dir"/$file
