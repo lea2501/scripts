3b599d3be569011aa70be392016f23246a3ad50b
Lea Chescotta <ochescotta@teco.com.ar>
2022-04-23 16:30:01 -0300

    Update arch linux scripts

--- devuan/post_install/as_root-install_amd_graphics_card.sh
+++ devuan/post_install/as_root-install_amd_graphics_card.sh
@@ -10,10 +10,10 @@ apt-get -y --fix-missing install amd64-microcode
 #apt-get -y --fix-missing install linux-image-amd64/chimaera-backports firmware-linux/chimaera-backports firmware-linux-nonfree/chimaera-backports firmware-amd-graphics/chimaera-backports libgl1-mesa-dri/chimaera-backports libglx-mesa0/chimaera-backports mesa-vulkan-drivers/chimaera-backports xserver-xorg-video-all/chimaera-backports
 apt-get -y --fix-missing install linux-image-amd64 firmware-linux firmware-linux-nonfree firmware-amd-graphics libgl1-mesa-dri libglx-mesa0 mesa-vulkan-drivers xserver-xorg-video-all
 
-echo > /etc/X11/xorg.conf.d/20-amdgpu.conf <<EOF
-Section "Device"
-   Identifier  "AMD Graphics"
-   Driver      "amdgpu"
-   Option      "TearFree"  "true"
-EndSection
-EOF
+{
+  echo 'Section "Device"'
+  echo '    Identifier  "AMD Graphics"'
+  echo '    Driver      "amdgpu"'
+  echo '    Option      "TearFree"  "true"'
+  echo 'EndSection'
+}>>/etc/X11/xorg.conf.d/20-amdgpu.conf
