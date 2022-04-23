bc63f986c1df6266c87940000c8de99466e09832
Lea Chescotta <ochescotta@teco.com.ar>
2022-04-23 10:30:42 -0300

    Updated devuan scripts

--- devuan/post_install/as_root-install_amd_graphics_card.sh
+++ devuan/post_install/as_root-install_amd_graphics_card.sh
@@ -5,4 +5,6 @@ set -e
 # debug log
 #set -x
 
-apt-get -y --fix-missing install linux-image/chimaera-backports firmware-linux/chimaera-backports firmware-linux-nonfree/chimaera-backports firmware-amd-graphics/chimaera-backports libgl1-mesa-dri/chimaera-backports libglx-mesa0/chimaera-backports mesa-vulkan-drivers/chimaera-backports xserver-xorg-video-all/chimaera-backports
+apt-get -y --fix-missing install amd64-microcode
+#apt-get -y --fix-missing install linux-image/chimaera-backports firmware-linux/chimaera-backports firmware-linux-nonfree/chimaera-backports firmware-amd-graphics/chimaera-backports libgl1-mesa-dri/chimaera-backports libglx-mesa0/chimaera-backports mesa-vulkan-drivers/chimaera-backports xserver-xorg-video-all/chimaera-backports
+apt-get -y --fix-missing install linux-image firmware-linux firmware-linux-nonfree firmware-amd-graphics libgl1-mesa-dri libglx-mesa0 mesa-vulkan-drivers xserver-xorg-video-all/chimaera-backports
