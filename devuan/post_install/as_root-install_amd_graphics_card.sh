416dfe8239a0476f46cce40ed6f34c835cb68900
Lea Chescotta <ochescotta@teco.com.ar>
2022-04-23 16:26:29 -0300

    Update arch linux scripts

--- devuan/post_install/as_root-install_amd_graphics_card.sh
+++ devuan/post_install/as_root-install_amd_graphics_card.sh
@@ -6,6 +6,14 @@ set -e
 #set -x
 
 apt-get -y --fix-missing install amd64-microcode
-apt-get -y --fix-missing install amd64-microcode/chimaera-backports
-#apt-get -y --fix-missing install linux-image/chimaera-backports firmware-linux/chimaera-backports firmware-linux-nonfree/chimaera-backports firmware-amd-graphics/chimaera-backports libgl1-mesa-dri/chimaera-backports libglx-mesa0/chimaera-backports mesa-vulkan-drivers/chimaera-backports xserver-xorg-video-all/chimaera-backports
-apt-get -y --fix-missing install linux-image firmware-linux firmware-linux-nonfree firmware-amd-graphics libgl1-mesa-dri libglx-mesa0 mesa-vulkan-drivers xserver-xorg-video-all/chimaera-backports
+#apt-get -y --fix-missing install amd64-microcode/chimaera-backports
+#apt-get -y --fix-missing install linux-image-amd64/chimaera-backports firmware-linux/chimaera-backports firmware-linux-nonfree/chimaera-backports firmware-amd-graphics/chimaera-backports libgl1-mesa-dri/chimaera-backports libglx-mesa0/chimaera-backports mesa-vulkan-drivers/chimaera-backports xserver-xorg-video-all/chimaera-backports
+apt-get -y --fix-missing install linux-image-amd64 firmware-linux firmware-linux-nonfree firmware-amd-graphics libgl1-mesa-dri libglx-mesa0 mesa-vulkan-drivers xserver-xorg-video-all
+
+echo > /etc/X11/xorg.conf.d/20-amdgpu.conf <<EOF
+Section "Device"
+   Identifier  "AMD Graphics"
+   Driver      "amdgpu"
+   Option      "TearFree"  "true"
+EndSection
+EOF
