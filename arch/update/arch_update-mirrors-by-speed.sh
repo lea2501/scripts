d5020a0717b0c5e15f2c40acc2b80196bb19d9f2
Lea Chescotta <ochescotta@teco.com.ar>
2022-04-23 11:21:52 -0300

    Update arch linux scripts

--- arch/update/arch_update-mirrors-by-speed.sh
+++ arch/update/arch_update-mirrors-by-speed.sh
@@ -10,8 +10,9 @@ if [[ -z $su ]]; then
   export su="sudo"
 fi
 
+$su pacman -Sy pacman-contrib
+
 ############ Script
-echo "Creating backup of existing mirrorlist..."
 FILE=/etc/pacman.d/mirrorlist.backup
 if [ -f "$FILE" ]; then
     echo "Mirrorlist $FILE backup file exists."
@@ -19,10 +20,7 @@ else
     echo "Mirrorlist $FILE backup file does not exists. Creating backup"
     $su cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 fi
-echo "Creating backup of existing mirrorlist... DONE"
 
-echo "Getting worldwide mirrorlist and sorting them by speed..."
 echo -e "\033[33;5m THIS WILL TAKE SEVERAL MINUTES... \033[0m"
 curl -s "https://archlinux.org/mirrorlist/?protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > ~/mirrorlist.new
 $su cp ~/mirrorlist.new /etc/pacman.d/mirrorlist
-echo "Getting worldwide mirrorlist and sorting them by speed... DONE"
