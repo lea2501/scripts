8c9254c3d96f41953093842398b51f00d9981d7e
Lea Chescotta <ochescotta@teco.com.ar>
2022-04-24 11:02:26 -0300

    Updated devuan install games script

--- devuan/games/install_repo_games.sh
+++ devuan/games/install_repo_games.sh
@@ -14,13 +14,13 @@ fi
 cd || return
 
 # roguelikes
-$su apt-get -y --fix-missing install moria angband nethack-console cataclysm-dda-curses games-rogue
+$su apt-get -y --fix-missing install moria angband nethack-console cataclysm-dda-curses #games-rogue
 
 # doom
 $su apt-get -y --fix-missing install chocolate-doom crispy-doom prboom-plus glbsp zdbsp
 
 # doom3
-$su apt-get -y --fix-missing install dhewm3 dhewm3-doom3 dhewm3-d3xp rbdoom3bfg
+$su apt-get -y --fix-missing install dhewm3 dhewm3-doom3 dhewm3-d3xp #rbdoom3bfg
 
 # quake
 $su apt-get -y --fix-missing install quakespasm
@@ -41,7 +41,7 @@ $su apt-get -y --fix-missing install rtcw
 $su apt-get -y --fix-missing install uqm
 
 # minetest
-$su apt-get -y --fix-missing install minetest minetest-server minetest-mod-basic-materials minetest-mod-homedecor minetest-mod-nether minetest-mod-player-3d-armor minetest-mod-mobs-redo
+$su apt-get -y --fix-missing install minetest minetest-server #minetest-mod-basic-materials minetest-mod-homedecor minetest-mod-nether minetest-mod-player-3d-armor minetest-mod-mobs-redo
 
 # tor
 $su apt-get -y --fix-missing install tor obfs4proxy
@@ -50,7 +50,7 @@ $su apt-get -y --fix-missing install tor obfs4proxy
 $su apt-get -y --fix-missing install torcs torcs-data
 
 # emulators
-$su apt-get -y --fix-missing install dosbox mgba-sdl mednafen mednaffe higan hatari scummvm scummvm-tools fs-uae fs-uae-launcher nestopia mame mame-tools pcsxr pcsx2
+$su apt-get -y --fix-missing install dosbox mednafen dolphin-emu #mgba-sdl mednaffe higan hatari scummvm scummvm-tools fs-uae fs-uae-launcher nestopia mame mame-tools pcsxr pcsx2
 
 # retroarch
 #$su apt-get -y --fix-missing install retroarch
