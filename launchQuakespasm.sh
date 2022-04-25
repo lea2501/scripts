7b99c95748740104dd6b7a5a791b4650a692a8e5
Lea Chescotta <ochescotta@teco.com.ar>
2022-04-24 22:25:27 -0300

    updated scripts

--- launchQuakespasm.sh
+++ launchQuakespasm.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-if [[ -z $1 ]] || [[ "$1" == "-h" ]]; then
+#!/bin/bash
+
+if { [ -z "$1" ] || [ "$1" = -h ] || [ "$1" = --help ];}; then
   echo "Usage:"
   echo "  $ $(basename $0) [map]"
   echo "  $ export retro=0 && $(basename $0) [map]"
@@ -10,18 +11,18 @@ if [[ -z $1 ]] || [[ "$1" == "-h" ]]; then
   echo ""
   exit
 fi
-if [[ "$1" == "--list" ]] || [[ "$1" == "-l" ]]; then
-  find ~/games/quake/*/{maps/*.bsp,pak0.pak} -type f
+if { [ "$1" = --list ] || [ "$1" = -l ];}; then
+  find "$HOME"/games/quake/*/{maps/*.bsp,pak0.pak} -type f
   echo ""
   exit
 fi
 
 # set default options
-if [[ -z $retro ]] || [[ "$retro" == "0" ]]; then
+if { [ -z "$retro" ] || [ "$retro" = "0" ];}; then
       export r_lerpmodels=1
       export r_lerpmove=1
       export r_scale=1
-  elif [[ "$retro" == "1" ]]; then
+  elif [ "$retro" = "1" ]]; then
       export r_lerpmodels=0
       export r_lerpmove=0
       export r_scale=4
@@ -34,7 +35,7 @@ mapdir=$(awk -F/ '{print $(NF-1)}' <<< "${mapdir}")
 mapname=$(basename -- "$1" ".bsp")
 
 if [ -f ~/src/quakespasm-quakespasm/Quake/quakespasm ]; then
-  cd ~/src/quakespasm-quakespasm/Quake/ $$ ./quakespasm -width 1920 -height 1080 -fullscreen -basedir ~/games/quake/ -heapsize 256000 -zone 4096 -game $mapdir +map $mapname +skill 1 +mlook +r_particles 2 +r_lerpmodels $r_lerpmodels +r_lerpmove $r_lerpmove +r_viewmodel_quake 1 +r_scale $r_scale +scr_ofsx -2.8 +scr_sbaralpha 1 +v_gunkick 2 +gamma 1.2 +contrast 1.5 +fov 85 +fog 0.02 -fitz
+  cd ~/src/quakespasm-quakespasm/Quake/ && ./quakespasm -width 1920 -height 1080 -fullscreen -basedir ~/games/quake/ -heapsize 256000 -zone 4096 -game $mapdir +map $mapname +skill 1 +mlook +r_particles 2 +r_lerpmodels $r_lerpmodels +r_lerpmove $r_lerpmove +r_viewmodel_quake 1 +r_scale $r_scale +scr_ofsx -2.8 +scr_sbaralpha 1 +v_gunkick 2 +gamma 1.2 +contrast 1.5 +fov 85 +fog 0.02 -fitz
 else
   quakespasm -width 1920 -height 1080 -fullscreen -basedir ~/games/quake/ -heapsize 256000 -zone 4096 -game $mapdir +map $mapname +skill 1 +mlook +r_particles 2 +r_lerpmodels $r_lerpmodels +r_lerpmove $r_lerpmove +r_viewmodel_quake 1 +r_scale $r_scale +scr_ofsx -2.8 +scr_sbaralpha 1 +v_gunkick 2 +gamma 1.2 +contrast 1.5 +fov 85 +fog 0.02 -fitz
 fi
---
+++ removeFirstCharsFromFilenamesInDir.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if { [ -z "$1" ] || [ "$1" = -h ] || [ "$1" = --help ];}; then
+  echo "Usage:"
+  echo "  $(basename "$0") [chars_to_remove]"
+  echo ""
+  exit
+fi
+
+amount=$1
+for i in *.*; do
+   newName=$(echo "$i" | cut -c"$amount"-)
+   mv -v "$i" "$newName"
+done
