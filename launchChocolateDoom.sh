77cf4bcc11739a3302b9e6712c64e3c5826385b5
Lea Chescotta <ochescotta@teco.com.ar>
2022-04-24 21:45:04 -0300

    Updated launch games scripts

--- launchChocolateDoom.sh
+++ launchChocolateDoom.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
-if [[ -z $1 ]] || [[ "$1" == "-h" ]]; then
+#!/bin/bash
+if { [ -z "$1" ] || [ "$1" = -h ] || [ "$1" = --help ];}; then
   echo "Usage:"
-  echo "  $(basename $0) [game] [map]"
+  echo "  $(basename "$0") [game] [map]"
   echo ""
   echo "To list all available game and maps:"
-  echo "  $(basename $0) --list|-l"
+  echo "  $(basename "$0") --list|-l"
   echo ""
   exit
 fi
-if [[ "$1" == "--list" ]] || [[ "$1" == "-l" ]]; then
+if { [ "$1" = --list ] || [ "$1" = -l ];}; then
   find ~/games/doom/wads/{doom,doom2,tnt,plutonia}/vanilla/*/*.wad ! -name *tex*.* ! -name *res*.* ! -name *fix.* ! -name *demo*.* ! -name *credits*.* -type f
   echo ""
   exit
