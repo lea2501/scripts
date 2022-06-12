7f8e4eafb1b469e730df2e431f36d3ded161c5bb
Lea Chescotta <ochescotta@teco.com.ar>
2022-06-12 08:45:55 -0300

    updated firefox user.js updater scripts

--- updateFirefoxUserJs.sh
+++ updateFirefoxUserJs.sh
@@ -5,16 +5,25 @@ set -e
 # debug log
 #set -x
 
-repo="https://github.com/arkenfox/user.js.git"
-
+dir=user.js
+repo="https://github.com/arkenfox/${dir}.git"
 mkdir -p ~/src
 cd ~/src || return
-if [ ! -d "$repo" ]; then
+if [ ! -d "$dir" ]; then
   git clone $repo
-  cd "$repo" || return
+  cd "$dir" || return
 else
-  cd "$repo" || return
+  cd "$dir" || return
   git pull
 fi
 
+# apply custom settings
+cp -v user.js user.js.bak
+sed -i 's/\/\/ user_pref("media.peerconnection.enabled", false);/ user_pref("media.peerconnection.enabled", false);/g' user.js
+sed -i 's/\/\/ user_pref("media.navigator.enabled", false);/ user_pref("media.navigator.enabled", false);/g' user.js
+sed -i 's/\/\/ user_pref("extensions.pocket.enabled", false);/ user_pref("extensions.pocket.enabled", false);/g' user.js
+
 cp -rv user.js ~/.mozilla/firefox/*default*/
+
+mv -v user.js user.js.new
+mv -v user.js.bak user.js
