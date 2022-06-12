c16551f93fdf3a12a1b8b1663e7abde421fb1de2
Lea Chescotta <ochescotta@teco.com.ar>
2022-06-12 08:46:21 -0300

    updated artix post install and connmanctl scripts

---
+++ arch/post_install/artix-loadkeys_at_boot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# fail if any commands fails
+#set -e
+# debug log
+#set -x
+
+# Set superuser privileges command if not set
+if [[ -z $su ]]; then
+  export su="sudo"
+fi
+
+# show available keymaps
+find /usr/share/kbd/keymaps/ -type f | grep la
+
+$su sed -i 's/keymap="us"/keymap="es"/g' /etc/conf.d/keymaps
+$su sed -i 's/KEYMAP=us/KEYMAP=es/g' /etc/vconsole.conf
