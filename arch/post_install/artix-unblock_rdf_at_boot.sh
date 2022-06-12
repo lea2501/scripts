87d0bfebba6bd10548c40dc36593210bfdbed220
Lea Chescotta <ochescotta@teco.com.ar>
2022-06-12 08:46:21 -0300

    updated artix post install and connmanctl scripts

---
+++ arch/post_install/artix-unblock_rdf_at_boot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# fail if any commands fails
+#set -e
+# debug log
+#set -x
+
+# Set superuser privileges command if not set
+if [[ -z $su ]]; then
+  export su="sudo"
+fi
+
+$su pacman -S tlp tlp-openrc
+
+$su rc-update add tlp default
+$su rc-service tlp start
---
+++ connmanctl_connect.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# fail if any commands fails
+set -e
+# debug log
+#set -x
+
+#doas rfkill unblock wifi
+#doas ip link set wlan0 up
+connmanctl scan wifi && wait $!
+connmanctl services && wait $!
+connmanctl connect wifi_085bd658a075_47616c6178792041333244313644_managed_psk
