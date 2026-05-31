16c43a54f4bd2dd9d546955386849ccca3afc922
Lea Hara Chescotta <lea.chescotta@tutanota.com>
2026-05-31 10:22:08 -0300

    updated games scripts

--- games/chocolate-doom_doom_vanilla.sh
+++ games/chocolate-doom_doom_vanilla.sh
@@ -5,15 +5,13 @@ game_dir="$HOME/games/doom"
 mod_files="$game_dir/mods/vanilla/sound/pk_doom_sfx/pk_doom_sfx_20120224.wad \
   $game_dir/mods/vanilla/palette/dimm_pal/doom-pal.wad"
 
-pwad_file=$(find "${game_dir}"/maps/${game}/vanilla/*/*.wad \
-   -ls | sort -rn | awk '{print $11}' | shuf -n 1)
+pwad_file=$(find "$game_dir"/maps/"$game"/vanilla \
+  -type f -name '*.wad' 2>/dev/null | shuf -n 1)
 
-set -x
-eval chocolate-doom -config "$game_dir"/config/chocolate/config.ini \
-  -fullscreen -iwad "$game_dir"/maps/iwads/${game}.wad \
-  -file "$pwad_file" "$mod_files" \
-  -savedir "$game_dir"/savegames/${game}/ \
+chocolate-doom -config "$game_dir"/config/chocolate/config.ini \
+  -fullscreen -iwad "$game_dir"/maps/iwads/"$game".wad \
+  -file "$pwad_file" $mod_files \
+  -savedir "$game_dir"/savegames/"$game"/ \
   -skill 3 \
   -warp 1 1 \
   > /tmp/chocolate-doom.log
-set +x
--- games/crispy-hexen_hexen_vanilla.sh
+++ games/crispy-hexen_hexen_vanilla.sh
@@ -4,17 +4,15 @@ game=hexen
 game_dir="$HOME/games/doom"
 mod_files="$game_dir/mods/vanilla/palette/dimm_pal/hex-pal.wad"
 
-pwad_file=$(find "${game_dir}"/maps/${game}/vanilla/*/*.wad \
-  "$game_dir"/maps/${game}/vanilla_cds/*/*.wad \
-  "$game_dir"/maps/${game}/nolimit/*/*.wad \
-   -ls | sort -rn | awk '{print $11}' | shuf -n 1)
+pwad_file=$(find "$game_dir"/maps/"$game"/vanilla \
+  "$game_dir"/maps/"$game"/vanilla_cds \
+  "$game_dir"/maps/"$game"/nolimit \
+  -type f -name '*.wad' 2>/dev/null | shuf -n 1)
 
-set -x
-eval crispy-hexen -config "$game_dir"/config/crispy/config_nolimit_hexen.ini \
-  -fullscreen -iwad "$game_dir"/maps/iwads/${game}.wad \
-  -file "$pwad_file" "$mod_files" \
-  -savedir "$game_dir"/savegames/${game}/ \
+crispy-hexen -config "$game_dir"/config/crispy/config_nolimit_hexen.ini \
+  -fullscreen -iwad "$game_dir"/maps/iwads/"$game".wad \
+  -file "$pwad_file" $mod_files \
+  -savedir "$game_dir"/savegames/"$game"/ \
   -skill 3 \
   -warp 1 1 \
   > /tmp/crispy-hexen.log
-set +x
